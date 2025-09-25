ca2a7745860f59248182854d45cee52eeeb863da
Chacha-bot-design <freechaaa@gmail.com>
2025-09-26 01:47:53 +0300

    Fix URL configuration to serve React properly

--- Frecha_Iotech/urls.py
+++ Frecha_Iotech/urls.py
@@ -1,15 +1,17 @@
 
 #############
-from django.urls import path, include
+
+
+
+
+###########
+# In urls.py
 from django.views.generic import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    # API routes
     path('api/providers/', include('store.urls')),
     path('api/bundles/', include('store.urls')),
     path('api/orders/', include('store.urls')),
-    
-    # Serve React for all other routes
     path('', TemplateView.as_view(template_name='index.html')),
-    path('<path:route>', TemplateView.as_view(template_name='index.html')),
-]
+] + staticfiles_urlpatterns()
