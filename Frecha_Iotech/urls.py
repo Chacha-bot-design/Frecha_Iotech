205d9b0304273bf7af19af1a076bade4d0814240
Chacha-bot-design <freechaaa@gmail.com>
2025-09-26 02:35:08 +0300

    Fix root URL routing to serve React index.html

--- Frecha_Iotech/urls.py
+++ Frecha_Iotech/urls.py
@@ -1,17 +1,23 @@
 
-
+########
 from django.contrib import admin
-from django.urls import path, include  # ← THIS LINE IS CRITICAL
+from django.urls import path, include
 from django.views.generic import TemplateView
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
+    
+    # API routes
     path('api/providers/', include('store.urls')),
     path('api/bundles/', include('store.urls')),
     path('api/orders/', include('store.urls')),
     
-    # Serve React for all routes starting with 'app/'
-    path('app/', TemplateView.as_view(template_name='index.html')),
-    path('app/<path:route>', TemplateView.as_view(template_name='index.html')),
+    # Serve React app - make sure this line is correct
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
+
+# Add static files serving
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
