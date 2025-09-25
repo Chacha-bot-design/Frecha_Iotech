75d0ab47ad125ed3396535146455740af53e234e
Chacha-bot-design <freechaaa@gmail.com>
2025-09-26 01:52:01 +0300

    Fix missing path import in urls.py

--- Frecha_Iotech/urls.py
+++ Frecha_Iotech/urls.py
@@ -1,11 +1,7 @@
 
-#############
 
-
-
-
-###########
-# In urls.py
+from django.contrib import admin
+from django.urls import path, include  # ← THIS LINE IS CRITICAL
 from django.views.generic import TemplateView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
@@ -14,4 +10,5 @@ urlpatterns = [
     path('api/bundles/', include('store.urls')),
     path('api/orders/', include('store.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
+    path('<path:route>', TemplateView.as_view(template_name='index.html')),
 ] + staticfiles_urlpatterns()
