680ee024bdb223db0cae49a5560ec0269f071e38
Chacha-bot-design <freechaaa@gmail.com>
2025-09-26 05:31:47 +0300

    Add JsonResponse import to fix manifest.json 500 error

--- Frecha_Iotech/urls.py
+++ Frecha_Iotech/urls.py
@@ -43,7 +43,3 @@ urlpatterns = [
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
 
-# Add static files serving (only in development)
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
