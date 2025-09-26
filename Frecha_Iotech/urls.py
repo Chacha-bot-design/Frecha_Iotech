1c1fa5c75ca9137828680e0d5da855046654c316
Chacha-bot-design <freechaaa@gmail.com>
2025-09-26 05:23:18 +0300

    Fix missing JsonResponse import in urls.py

--- Frecha_Iotech/urls.py
+++ Frecha_Iotech/urls.py
@@ -1,11 +1,10 @@
-
-########
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path  # ← Add re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views import View
+from django.http import JsonResponse  # ← ADD THIS MISSING IMPORT
 
 class ManifestView(View):
     def get(self, request):
@@ -20,12 +19,12 @@ class ManifestView(View):
             "icons": [
                 {
                     "src": "/static/favicon.ico",
-                    "sizes": "64x64",
+                    "sizes": "64x64", 
                     "type": "image/x-icon"
                 }
             ]
         }
-        return JsonResponse(manifest_data)
+        return JsonResponse(manifest_data)  # ← Now this will work!
 
 
 urlpatterns = [
@@ -37,12 +36,14 @@ urlpatterns = [
     path('api/bundles/', include('store.urls')),
     path('api/orders/', include('store.urls')),
 
+    # Manifest route
     path('manifest.json', ManifestView.as_view(), name='manifest'),
     
-    # Serve React app - make sure this line is correct
-    path('', TemplateView.as_view(template_name='index.html')),
+    # Serve React app - use re_path for catch-all
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
 
-# Add static files serving
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Add static files serving (only in development)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
