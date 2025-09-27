4432dc3553e46d7a92c05199a2d8c971f4f397d5
Chacha-bot-design <freechaaa@gmail.com>
2025-09-27 19:50:18 +0300

    changes

--- Frecha_Iotech/urls.py
+++ Frecha_Iotech/urls.py
@@ -3,6 +3,33 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+import os
+
+def debug_static_files(request):
+    """Debug view to check static files"""
+    static_info = {
+        'static_root': settings.STATIC_ROOT,
+        'static_root_exists': os.path.exists(settings.STATIC_ROOT),
+        'debug': settings.DEBUG,
+    }
+    
+    # Check specific files
+    files_to_check = [
+        'js/main.add70ce6.js',
+        'css/main.21a10a42.css',
+    ]
+    
+    for file_path in files_to_check:
+        full_path = os.path.join(settings.STATIC_ROOT, file_path)
+        static_info[file_path] = {
+            'exists': os.path.exists(full_path),
+            'path': full_path,
+            'size': os.path.getsize(full_path) if os.path.exists(full_path) else 0
+        }
+    
+    return JsonResponse(static_info)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
