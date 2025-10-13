86b6492483e3c90ad89cdc281c9785f4ddc446f9
Chacha-bot-design <freechaaa@gmail.com>
2025-10-14 00:18:22 +0300

    changes

--- frecha_iotechi/src/services/api.js
+++ frecha_iotechi/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://frecha-iotech.com/api';
+const API_BASE_URL = 'https://frecha-iotech.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
