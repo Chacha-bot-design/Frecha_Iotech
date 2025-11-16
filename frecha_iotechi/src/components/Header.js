fa1e7bb3e2136037217446aba8bcdda68bc64f8e
Chacha-bot-design <freechaaa@gmail.com>
2025-11-16 23:05:31 +0300

    Fix migration conflict

--- frecha_iotechi/src/components/Header.js
+++ frecha_iotechi/src/components/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
             <li><a href="#services">Services</a></li>
             <li><a href="#products">Products</a></li>
             <li><a href="#order">Order</a></li>
-             <li><a href="/track-order">Track Order</li>
+             <li><a href="/track-order">Track Order</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
         </nav>
